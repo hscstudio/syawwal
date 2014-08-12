3c6f75584b949f0a2493ee9f053d3562ec8ce3bf
Fajar Wahyu <fajarwahyuabdillah@gmail.com>
2014-08-12 10:04:41 +0700

    Memindah config param ke common semua

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,5 +1,3 @@
 <?php
 return [
-    'adminEmail' => 'sisfotik@gmail.com', // fajar - ganti email pake punya sisfotik
-    'namaAplikasi' => 'SIM BPPK' // fajar - nama aplikasi ditaruh di config. No hard code! CP: Yii::$app->params['namaAplikasi']
 ];
--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'sisfotik@gmail.com', // fajar - ganti email pake punya sisfotik
+    'supportEmail' => 'sisfotik@gmail.com', // fajar - ganti email pake punya sisfotik
     'user.passwordResetTokenExpire' => 3600,
+    'namaAplikasi' => 'SIM BPPK' // fajar - nama aplikasi ditaruh di config. Cara: Yii::$app->params['namaAplikasi']
 ];
