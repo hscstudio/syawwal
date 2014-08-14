ca2832d15535d1065cab3c5f929212e0ed0cb520
muhamadg <muhamadg@gmail.com>
2014-08-14 10:18:19 +0700

    nambahain path

--- backend/modules/pusdiklat/evaluation/Module.php
+++ backend/modules/pusdiklat/evaluation/Module.php
@@ -15,13 +15,13 @@ class Module extends \yii\base\Module
 		return [
 			['icon'=>'fa fa-fw fa-dashboard','label' => 'Dashboard', 'url' => ['/'.$this->uniqueId.'/default']],
 			['icon'=>'fa fa-fw fa-check-square-o','label' => 'Evaluasi Diklat', 'url' => ['#'],'items'=>[
-				['icon'=>'fa fa-fw fa-book', 'label' => 'Training', 'url' => ['/'.$this->uniqueId.'/training/index']],
+				['icon'=>'fa fa-fw fa-book', 'label' => 'Training', 'url' => ['/'.$this->uniqueId.'/training/index'],'path'=>'training']
 			]],
 			['icon'=>'fa fa-fw fa-cloud','label' => 'PHD', 'url' => ['#'],'items'=>[
-				['icon'=>'fa fa-fw fa-stack-overflow', 'label' => 'Training2', 'url' => ['/'.$this->uniqueId.'/training2/index']],
+				['icon'=>'fa fa-fw fa-stack-overflow', 'label' => 'Training2', 'url' => ['/'.$this->uniqueId.'/training2/index'],'path'=>'training2']
 			]],
 			['icon'=>'fa fa-fw fa-book','label' => 'IPK', 'url' => ['#'],'items'=>[
-				['icon'=>'fa fa-fw fa-stack-exchange', 'label' => 'Training3', 'url' => ['/'.$this->uniqueId.'/training3/index']],
+				['icon'=>'fa fa-fw fa-stack-exchange', 'label' => 'Training3', 'url' => ['/'.$this->uniqueId.'/training3/index'],'path'=>'training3']
 			]],
 		];
 	}
