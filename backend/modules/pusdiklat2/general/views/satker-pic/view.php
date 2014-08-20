056030408a6297f6dead3bc4de1228a897857f28
muhamadg <muhamadg@gmail.com>
2014-08-20 10:16:55 +0700

    Update view&form Program

--- backend/modules/pusdiklat2/general/views/satker-pic/view.php
+++ backend/modules/pusdiklat2/general/views/satker-pic/view.php
@@ -4,22 +4,22 @@ use yii\helpers\Html;
 use kartik\detail\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\Program */
+/* @var $model backend\models\SatkerPic */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Programs', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Satker Pics', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 $controller = $this->context;
 $menus = $controller->module->getMenuItems();
 $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
 ?>
-<div class="program-view">
+<div class="satker-pic-view">
 
     <?= DetailView::widget([
         'model' => $model,
 		'mode'=>DetailView::MODE_VIEW,
 		'panel'=>[
-			'heading'=>'<i class="fa fa-fw fa-globe"></i> '.'Programs # ' . $model->id,
+			'heading'=>'<i class="fa fa-fw fa-globe"></i> '.'Satker Pics # ' . $model->id,
 			'type'=>DetailView::TYPE_DEFAULT,
 		],
 		'buttons1'=> Html::a('<i class="fa fa-fw fa-arrow-left"></i>',['index'],
@@ -30,34 +30,16 @@ $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
 						['class'=>'btn btn-xs btn-danger kv-btn-delete',
 						 'title'=>'Delete', 'data-method'=>'post', 'data-confirm'=>'Are you sure you want to delete this item?']),
         'attributes' => [
-            //'id',
+            'id',
             [
 				'attribute' => 'ref_satker_id',
 				'value' => $model->satker->name,
 			],
-           // 'ref_satker_id',
-            'number',
+            'ref_satker_id',
+            'code',
             'name',
-            'hours',
-            'days',
-            //'test',
-			[
-				'attribute' => 'test',
-				'value' => $model->test == '0'?'Off':'On',
-			],
-            //'type',
-			[
-				'attribute' => 'type',
-				'value' => $model->type == '0'?'Lulus':'Mengikuti',
-			],
-            'note',
-            'validationStatus',
-            'validationNote',
-            //'status',
-			[
-				'attribute' => 'status',
-				'value' => $model->test == '0'?'Off':'On',
-			],
+            'value',
+            'status',
             'created',
             'createdBy',
             'modified',
--- backend/modules/pusdiklat2/general/views/training/view.php
+++ backend/modules/pusdiklat2/general/views/training/view.php
@@ -4,22 +4,22 @@ use yii\helpers\Html;
 use kartik\detail\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model backend\models\Program */
+/* @var $model backend\models\Training */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Programs', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Trainings', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 $controller = $this->context;
 $menus = $controller->module->getMenuItems();
 $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
 ?>
-<div class="program-view">
+<div class="training-view">
 
     <?= DetailView::widget([
         'model' => $model,
 		'mode'=>DetailView::MODE_VIEW,
 		'panel'=>[
-			'heading'=>'<i class="fa fa-fw fa-globe"></i> '.'Programs # ' . $model->id,
+			'heading'=>'<i class="fa fa-fw fa-globe"></i> '.'Trainings # ' . $model->id,
 			'type'=>DetailView::TYPE_DEFAULT,
 		],
 		'buttons1'=> Html::a('<i class="fa fa-fw fa-arrow-left"></i>',['index'],
@@ -30,40 +30,44 @@ $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
 						['class'=>'btn btn-xs btn-danger kv-btn-delete',
 						 'title'=>'Delete', 'data-method'=>'post', 'data-confirm'=>'Are you sure you want to delete this item?']),
         'attributes' => [
-            //'id',
+            'id',
+            [
+				'attribute' => 'tb_program_id',
+				'value' => $model->program->name,
+			],
+            'tb_program_id',
+            'revision',
             [
 				'attribute' => 'ref_satker_id',
 				'value' => $model->satker->name,
 			],
-           // 'ref_satker_id',
-            'number',
+            'ref_satker_id',
             'name',
-            'hours',
-            'days',
-            //'test',
-			[
-				'attribute' => 'test',
-				'value' => $model->test == '0'?'Off':'On',
-			],
-            //'type',
-			[
-				'attribute' => 'type',
-				'value' => $model->type == '0'?'Lulus':'Mengikuti',
-			],
+            'start',
+            'finish',
             'note',
-            'validationStatus',
-            'validationNote',
-            //'status',
-			[
-				'attribute' => 'status',
-				'value' => $model->test == '0'?'Off':'On',
-			],
+            'studentCount',
+            'classCount',
+            'executionSK',
+            'resultSK',
+            'costPlan',
+            'costRealisation',
+            'sourceCost',
+            'hostel',
+            'reguler',
+            'stakeholder',
+            'location',
+            'status',
             'created',
             'createdBy',
             'modified',
             'modifiedBy',
             'deleted',
             'deletedBy',
+            'approvedStatus',
+            'approvedStatusNote',
+            'approvedStatusDate',
+            'approvedStatusBy',
         ],
     ]) ?>
 
