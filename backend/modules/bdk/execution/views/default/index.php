b34689b08d97f47ea6787e13b03fc7e0d79d0e84
Fajar Wahyu <fajarwahyuabdillah@gmail.com>
2014-09-10 17:32:16 +0700

    Add chart unit spread in dashboard execution BDK

--- backend/modules/bdk/execution/views/default/index.php
+++ backend/modules/bdk/execution/views/default/index.php
@@ -6,25 +6,64 @@ $menus = $controller->module->getMenuItems();
 $this->params['sideMenu'][$controller->module->uniqueId]=$menus;
 ?>
 <div class="bdk-execution-default-index">
-    <?php
-        echo Highcharts::widget([
-           'options' => [
-                'chart' => [
-                    'type' => 'pie',
-                ],
-
-              'title' => ['text' => 'Fruit Consumption'],
-              'xAxis' => [
-                 'categories' => ['Apples', 'Bananas', 'Oranges']
-              ],
-              'yAxis' => [
-                 'title' => ['text' => 'Fruit eaten']
-              ],
-              'series' => [
-                 ['name' => 'Jane', 'data' => [1, 0, 4]],
-                 ['name' => 'John', 'data' => [5, 7, 3]]
-              ]
-           ]
-        ]);
-    ?>
+    <div class="row">
+        <div class="col-md-8">
+            <?php
+                echo Highcharts::widget([
+                   'options' => [
+                        'chart' => [
+                            'type' => 'column',
+                        ],
+                        'credits' => [
+                            'enable' => false
+                        ],
+                        'title' => [
+                            'text' => 'Total Rencana Sebaran Data Peserta Diklat'
+                        ],
+                        'xAxis' => [
+                            'categories' => $cat
+                        ],
+                        'yAxis' => [
+                            'title' => [
+                                'text' => 'Spread'
+                            ]
+                        ],
+                        'tooltip' => [
+                            'crosshairs' => [false, true]
+                        ],
+                        'series' => $series
+                   ]
+                ]);
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php
+                /*echo Highcharts::widget([
+                   'options' => [
+                        'chart' => [
+                            'type' => 'column',
+                        ],
+                        'credits' => [
+                            'enable' => false
+                        ],
+                        'title' => [
+                            'text' => 'Total Anggaran dan Realisasi'
+                        ],
+                        'xAxis' => [
+                            'categories' => $catYear
+                        ],
+                        'yAxis' => [
+                            'title' => [
+                                'text' => 'Rupiah'
+                            ]
+                        ],
+                        'tooltip' => [
+                            'crosshairs' => [false, true]
+                        ],
+                        'series' => $seriesCost
+                   ]
+                ]);*/
+            ?>
+        </div>
+    </div>
 </div>
