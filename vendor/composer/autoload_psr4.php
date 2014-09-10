a9986bff19ba77ec65f8232435681bb9f89ee23a
Fajar Wahyu <fajarwahyuabdillah@gmail.com>
2014-09-10 12:17:15 +0700

    Add vendor miloschuman: highcharts

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -15,6 +15,7 @@ return array(
     'yii\\bootstrap\\' => array($vendorDir . '/yiisoft/yii2-bootstrap'),
     'yii\\authclient\\' => array($vendorDir . '/yiisoft/yii2-authclient'),
     'yii\\' => array($vendorDir . '/yiisoft/yii2'),
+    'miloschuman\\highcharts\\' => array($vendorDir . '/miloschuman/yii2-highcharts-widget/src'),
     'mdm\\admin\\' => array($vendorDir . '/mdmsoft/yii2-admin'),
     'kartik\\' => array($vendorDir . '/kartik-v/yii2-helpers', $vendorDir . '/kartik-v/bootstrap-popover-x', $vendorDir . '/kartik-v/dependent-dropdown', $vendorDir . '/kartik-v/bootstrap-star-rating', $vendorDir . '/kartik-v/yii2-field-range', $vendorDir . '/kartik-v/yii2-widgets', $vendorDir . '/kartik-v/yii2-popover-x', $vendorDir . '/kartik-v/yii2-editable', $vendorDir . '/kartik-v/bootstrap-checkbox-x', $vendorDir . '/kartik-v/yii2-checkbox-x', $vendorDir . '/kartik-v/yii2-money', $vendorDir . '/kartik-v/yii2-slider', $vendorDir . '/kartik-v/yii2-grid', $vendorDir . '/kartik-v/php-date-formatter', $vendorDir . '/kartik-v/yii2-datecontrol', $vendorDir . '/kartik-v/yii2-icons', $vendorDir . '/kartik-v/yii2-builder', $vendorDir . '/kartik-v/yii2-detail-view', $vendorDir . '/kartik-v/bootstrap-fileinput'),
     'hscstudio\\heart\\' => array($vendorDir . '/hscstudio/yii2-heart'),
